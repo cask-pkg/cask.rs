f4350da01cf02b806e7b0590863aa3196cc2e37a
Axetroy <axetroy.dev@gmail.com>
2022-03-11 22:57:54 +0800

    update

--- src/download.rs
+++ src/download.rs
@@ -1,14 +1,16 @@
 use core::result::Result;
 use eyre::Report;
 use std::fs::File;
-use std::io::copy;
 use std::path::Path;
 
-pub fn download(url: &str, dest: &Path) -> Result<(), Report> {
-    let resp = reqwest::blocking::get(url)?;
-    let content = resp.text()?;
-    let mut dest = File::create(dest)?;
-    copy(&mut content.as_bytes(), &mut dest)?;
+pub fn download(url: &str, filepath: &Path) -> Result<(), Report> {
+    let mut resp = reqwest::blocking::get(url)?;
+
+    assert_eq!(resp.status(), 200);
+
+    let mut dest = File::create(filepath)?;
+
+    resp.copy_to(&mut dest)?;
 
     Ok(())
 }

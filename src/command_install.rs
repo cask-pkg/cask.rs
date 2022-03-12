f5333310a6e921929f5b2bf3ea76ddc88383679e
Axetroy <axetroy.dev@gmail.com>
2022-03-12 15:31:21 +0800

    update

--- src/command_install.rs
+++ src/command_install.rs
@@ -1,19 +1,25 @@
 #![deny(warnings)]
 
+extern crate flate2;
+extern crate tar;
+
 use crate::formula;
 use crate::git;
 use crate::util;
 use crate::util::iso8601;
 
 use eyre::Report;
+use flate2::read::GzDecoder;
 use sha2::{Digest, Sha256};
 use std::env;
 use std::fs;
-use std::fs::File;
+use std::fs::{set_permissions, File};
+use std::io;
 use std::io::BufReader;
 use std::io::Read;
 use std::io::Write;
 use std::time::{SystemTime, UNIX_EPOCH};
+use tar::Archive;
 
 pub async fn install(package_name: &str) -> Result<(), Report> {
     let url = format!("https://{}-cask.git", package_name);
@@ -151,11 +157,78 @@ created_at = "{}"
         None => Err(eyre::format_err!("{} not support your arch", package_name)),
     }?;
 
-    let tar_file_path = package_dir
+    let tar_file_path = &package_dir
         .join("version")
         .join(format!("{}.tar.gz", package_formula.package.version));
+    let tar_file_name = tar_file_path.file_name().unwrap().to_str().unwrap();
 
-    util::download(&arch.url, &tar_file_path).await?;
+    util::download(&arch.url, tar_file_path).await?;
+
+    let tar_file = File::open(tar_file_path)?;
+
+    let bin_name = if cfg!(target_os = "windows") {
+        format!("{}.exe", package_formula.package.bin)
+    } else {
+        package_formula.package.bin
+    };
+
+    let mut bin_found = false;
+
+    let output_file_path = package_dir.join("bin").join(&bin_name);
+
+    // .tar.gz
+    if tar_file_name.ends_with(".tar.gz") {
+        let tar = GzDecoder::new(&tar_file);
+        let mut archive = Archive::new(tar);
+
+        let files = archive.entries()?;
+
+        for e in files {
+            let mut entry = e?;
+
+            let entry_file = entry.path()?;
+
+            if let Some(file_name) = entry_file.file_name() {
+                if file_name.to_str().unwrap() == bin_name {
+                    entry.unpack(&output_file_path)?;
+                    bin_found = true;
+                    break;
+                }
+            }
+        }
+    } else if tar_file_name.ends_with(".zip") {
+        let mut archive = zip::ZipArchive::new(&tar_file)?;
+
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i)?;
+
+            if file.is_file() && file.name() == bin_name {
+                let mut output_file = File::create(&output_file_path)?;
+
+                io::copy(&mut file, &mut output_file)?;
+
+                bin_found = true;
+
+                // Get and Set permissions
+                #[cfg(unix)]
+                {
+                    use std::os::unix::fs::PermissionsExt;
+
+                    if let Some(mode) = file.unix_mode() {
+                        set_permissions(&output_file_path, fs::Permissions::from_mode(mode))?;
+                    }
+                }
+                break;
+            }
+        }
+    }
+
+    if !bin_found {
+        return Err(eyre::format_err!(
+            "can not found binary file '{}' in tar",
+            bin_name
+        ));
+    }
 
     Ok(())
 }

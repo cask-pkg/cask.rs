4cdc65d2f0898961b90d7dd4cfedc5dee99cf11d
Axetroy <axetroy.dev@gmail.com>
2022-03-13 18:52:18 +0800

    test: add more test for extractor

--- src/extractor.rs
+++ src/extractor.rs
@@ -114,8 +114,58 @@ mod tests {
 
         let extracted_file_path = dest_dir.join("test");
 
-        let meta = fs::metadata(extracted_file_path).unwrap();
+        let meta = fs::metadata(&extracted_file_path).unwrap();
 
         assert_eq!(meta.len(), 12);
+
+        let content = fs::read_to_string(&extracted_file_path).unwrap();
+
+        assert_eq!(content, "hello world\n");
+
+        fs::remove_file(&extracted_file_path).unwrap();
+    }
+
+    #[test]
+    fn test_extract_tar_if_bin_not_exist() {
+        let extractor_dir = env::current_dir()
+            .unwrap()
+            .join("fixtures")
+            .join("extractor");
+
+        let tar_file_path = extractor_dir.join("test.tar");
+
+        let dest_dir = extractor_dir;
+
+        let r = extractor::extract(&tar_file_path, "not_exist", &dest_dir);
+
+        assert!(r.is_err());
+    }
+
+    #[test]
+    fn test_extract_tar_gz() {
+        let extractor_dir = env::current_dir()
+            .unwrap()
+            .join("fixtures")
+            .join("extractor");
+
+        let tar_file_path = extractor_dir.join("test.tar.gz");
+
+        let dest_dir = extractor_dir;
+
+        let r = extractor::extract(&tar_file_path, "test", &dest_dir);
+
+        assert!(r.is_ok());
+
+        let extracted_file_path = dest_dir.join("test");
+
+        let meta = fs::metadata(&extracted_file_path).unwrap();
+
+        assert_eq!(meta.len(), 12);
+
+        let content = fs::read_to_string(&extracted_file_path).unwrap();
+
+        assert_eq!(content, "hello world\n");
+
+        fs::remove_file(&extracted_file_path).unwrap();
     }
 }

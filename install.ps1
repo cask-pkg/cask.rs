34d474b5033aa645845916150c4eec4b3d5229c2
Axetroy <axetroy.dev@gmail.com>
2022-03-16 16:04:47 +0800

    chore: add install script

---
+++ install.ps1
@@ -0,0 +1,78 @@
+#!/usr/bin/env pwsh
+
+$ErrorActionPreference = 'Stop'
+
+$githubUrl = "https://github.com"
+$owner = "axetroy"
+$repoName = "cask.rs"
+$exeName = "cask"
+$version = ""
+
+if ([Environment]::Is64BitProcess) {
+  $arch = "x86_64"
+} else {
+  $arch = "x86"
+}
+
+$BinDir = "$Home\bin"
+$downloadedTagGz = "$BinDir\${exeName}.tar.gz"
+$downloadedExe = "$BinDir\${exeName}.exe"
+$fileName = "${exeName}_${arch_}-pc-windows-msvc"
+
+# GitHub requires TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$ResourceUri = if (!$version) {
+  "${githubUrl}/${owner}/${repoName}/releases/latest/download/${fileName}.tar.gz"
+} else {
+  "${githubUrl}/${owner}/${repoName}/releases/download/${Version}/${fileName}.tar.gz"
+}
+
+if (!(Test-Path $BinDir)) {
+  New-Item $BinDir -ItemType Directory | Out-Null
+}
+
+Invoke-WebRequest $ResourceUri -OutFile $downloadedTagGz -UseBasicParsing -ErrorAction Stop
+
+function Check-Command {
+  param($Command)
+  $found = $false
+  try
+  {
+      $Command | Out-Null
+      $found = $true
+  }
+  catch [System.Management.Automation.CommandNotFoundException]
+  {
+      $found = $false
+  }
+
+  $found
+}
+
+if (Check-Command -Command tar) {
+  Invoke-Expression "tar -xvzf $downloadedTagGz -C $BinDir"
+} else {
+  function Expand-Tar($tarFile, $dest) {
+
+      if (-not (Get-Command Expand-7Zip -ErrorAction Ignore)) {
+          Install-Package -Scope CurrentUser -Force 7Zip4PowerShell > $null
+      }
+
+      Expand-7Zip $tarFile $dest
+  }
+
+  Expand-Tar $downloadedTagGz $BinDir
+}
+
+Remove-Item $downloadedTagGz
+
+$User = [EnvironmentVariableTarget]::User
+$Path = [Environment]::GetEnvironmentVariable('Path', $User)
+if (!(";$Path;".ToLower() -like "*;$BinDir;*".ToLower())) {
+  [Environment]::SetEnvironmentVariable('Path', "$Path;$BinDir", $User)
+  $Env:Path += ";$BinDir"
+}
+
+Write-Output "${exeName} was installed successfully to $downloadedExe"
+Write-Output "Run '${exeName} --help' to get started"
---
+++ install.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+
+set -e
+
+owner="axetroy"
+repo="cask.rs"
+exe_name="cask"
+githubUrl="https://github.com"
+version="" # auto install the latest version
+
+get_arch() {
+    # darwin/amd64: Darwin axetroydeMacBook-Air.local 20.5.0 Darwin Kernel Version 20.5.0: Sat May  8 05:10:33 PDT 2021; root:xnu-7195.121.3~9/RELEASE_X86_64 x86_64
+    # linux/amd64: Linux test-ubuntu1804 5.4.0-42-generic #46~18.04.1-Ubuntu SMP Fri Jul 10 07:21:24 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux
+    a=$(uname -m)
+    case ${a} in
+        "x86_64" | "amd64" )
+            echo "x86_64"
+        ;;
+        "aarch64" | "arm64")
+            echo "aarch64"
+        ;;
+        "mips64el")
+            echo "mips64el"
+        ;;
+        "mips64")
+            echo "mips64"
+        ;;
+        *)
+            echo ${NIL}
+        ;;
+    esac
+}
+
+get_vendor(){
+    # darwin: Darwin
+    os=$(get_os)
+    case ${os} in
+        "darwin" )
+            echo "apple"
+        ;;
+        *)
+            echo "unknown"
+        ;;
+    esac
+}
+
+get_os(){
+    # darwin: Darwin
+    # linux: Linux
+    os=$(uname -s | awk '{print tolower($0)}')
+}
+
+get_abi(){
+    vendor=$(get_vendor)
+    case ${os} in
+        "apple" )
+            echo ""
+        ;;
+        *)
+            echo "-musl"
+        ;;
+    esac
+}
+
+downloadFolder="${HOME}/Downloads"
+mkdir -p ${downloadFolder} # make sure download folder exists
+os=$(get_os)
+arch=$(get_arch)
+file_name="${exe_name}_${arch}-${vendor}${abi}.tar.gz" # the file name should be download
+downloaded_file="${downloadFolder}/${file_name}" # the file path should be download
+executable_folder="/usr/local/bin" # Eventually, the executable file will be placed here
+
+# if version is empty
+if [ -z "$version" ]; then
+    asset_path=$(
+        command curl -sSf ${githubUrl}/${owner}/${repo}/releases |
+        command grep -o "/${owner}/${repo}/releases/download/.*/${file_name}" |
+        command head -n 1
+    )
+    if [[ ! "$asset_path" ]]; then exit 1; fi
+    asset_uri="${githubUrl}${asset_path}"
+else
+    asset_uri="${githubUrl}/${owner}/${repo}/releases/download/${version}/${file_name}"
+fi
+
+echo "[1/3] Download ${asset_uri} to ${downloadFolder}"
+rm -f ${downloaded_file}
+curl --fail --location --output "${downloaded_file}" "${asset_uri}"
+
+echo "[2/3] Install ${exe_name} to the ${executable_folder}"
+tar -xz -f ${downloaded_file} -C ${executable_folder}
+exe=${executable_folder}/${exe_name}
+chmod +x ${exe}
+
+echo "[3/3] Set environment variables"
+echo "${exe_name} was installed successfully to ${exe}"
+if command -v $exe_name --version >/dev/null; then
+    echo "Run '$exe_name --help' to get started"
+else
+    echo "Manually add the directory to your \$HOME/.bash_profile (or similar)"
+    echo "  export PATH=${executable_folder}:\$PATH"
+    echo "Run '$exe_name --help' to get started"
+fi
+
+exit 0

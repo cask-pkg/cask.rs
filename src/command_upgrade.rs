abd5be102f280d1a7f7cb0fccf6b41bf70942c80
Axetroy <axetroy.dev@gmail.com>
2022-03-16 19:51:28 +0800

    fix: upgrade command use semver to compare version, close #5

--- src/command_upgrade.rs
+++ src/command_upgrade.rs
@@ -5,6 +5,7 @@ use crate::command_install;
 use crate::formula;
 
 use eyre::Report;
+use semver::Version;
 
 pub async fn upgrade(cask: &cask::Cask, package_name: &str) -> Result<(), Report> {
     let package_formula = cask.package_formula(package_name)?;
@@ -12,6 +13,9 @@ pub async fn upgrade(cask: &cask::Cask, package_name: &str) -> Result<(), Report
     let cask_info = package_formula.cask.ok_or_else(|| {
         eyre::format_err!("can not parse cask property of file '{}'", package_name)
     })?;
+
+    let current = Version::parse(&cask_info.version)
+        .map_err(|e| eyre::format_err!("invalid semver version '{}': {}", &cask_info.version, e))?;
 
     let remote_formula = formula::fetch(cask, package_name, true)?;
 
@@ -22,14 +26,17 @@ pub async fn upgrade(cask: &cask::Cask, package_name: &str) -> Result<(), Report
         ));
     }
 
-    let latest = &remote_formula.package.versions[0];
+    let latest_str = &remote_formula.package.versions[0];
 
-    if latest == cask_info.version.as_str() {
+    let latest = Version::parse(latest_str)
+        .map_err(|e| eyre::format_err!("invalid semver version '{}': {}", latest_str, e))?;
+
+    if latest.gt(&current) {
         eprintln!("You have used the latest version of '{}'", package_name);
         return Ok(());
     }
 
-    command_install::install(cask, package_name, Some(latest)).await?;
+    command_install::install(cask, package_name, Some(latest_str)).await?;
 
     eprintln!(
         "Upgrade {}@{} from  to '{}' finish!",

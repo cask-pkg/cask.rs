99ad6e186cc2cf0e6e000893b26e21d5b04bf066
Axetroy <axetroy.dev@gmail.com>
2022-03-19 11:15:15 +0800

    feat: support short name for cask info. close #22

--- src/command_info.rs
+++ src/command_info.rs
@@ -5,36 +5,54 @@ use crate::cask;
 use eyre::Report;
 
 pub async fn info(cask: &cask::Cask, package_name: &str) -> Result<(), Report> {
-    let package_dir = cask.package_dir(package_name);
-    let package_formula = &cask.package_formula(package_name)?;
+    let packages = cask.list_formula()?;
 
-    let cask_info = &package_formula.cask.as_ref().ok_or_else(|| {
-        eyre::format_err!("can not parse cask property of file '{}'", package_name)
-    })?;
+    let package = packages
+        .iter()
+        .find(|p| p.package.name == package_name)
+        .or_else(|| packages.iter().find(|p| p.package.bin == package_name));
 
-    let remote_versions = &package_formula.get_versions()?;
+    if let Some(package_formula) = package {
+        let cask_info = &package_formula.cask.as_ref().ok_or_else(|| {
+            eyre::format_err!("can not parse cask property of file '{}'", package_name)
+        })?;
 
-    let mut msg = format!(
-        r#"{}
+        let remote_versions = &package_formula.get_versions()?;
+
+        let mut msg = format!(
+            r#"{}
 Package: {}
 Version: {}
 Repository: {}
 Location: {}
 Remote Versions:
 "#,
-        package_formula.package.description,
-        cask_info.name,
-        cask_info.version,
-        package_formula.package.repository,
-        package_dir.display()
-    );
+            package_formula.package.description,
+            cask_info.name,
+            cask_info.version,
+            package_formula.package.repository,
+            package_formula
+                .filepath
+                .parent()
+                .ok_or_else(|| eyre::format_err!(
+                    "can not get parent folder of '{}'",
+                    package_formula.filepath.display()
+                ))?
+                .display()
+        );
 
-    for v in remote_versions {
-        msg.push_str(v);
-        msg.push('\n');
+        for v in remote_versions {
+            msg.push_str(v);
+            msg.push('\n');
+        }
+
+        print!("{}", msg);
+
+        Ok(())
+    } else {
+        return Err(eyre::format_err!(
+            "can not found the installed package '{}'",
+            package_name
+        ));
     }
-
-    print!("{}", msg);
-
-    Ok(())
 }

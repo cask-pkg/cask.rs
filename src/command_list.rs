804d4efdd801b1fce9c5d207936a6cadf107a087
Axetroy <axetroy.dev@gmail.com>
2022-03-19 23:06:47 +0800

    refactor: improve `cask ls` output. print pretty table instead of raw

--- src/command_list.rs
+++ src/command_list.rs
@@ -2,10 +2,22 @@
 
 use crate::cask;
 
+use chrono::prelude::*;
 use eyre::Report;
+use tabled::{Style, Table, Tabled};
+
+#[derive(Tabled)]
+struct PackageInfo {
+    name: String,
+    bin: String,
+    version: String,
+    install_at: String,
+}
 
 pub async fn list(cask: &cask::Cask) -> Result<(), Report> {
     cask.init()?;
+
+    let mut packages: Vec<PackageInfo> = vec![];
 
     for package in cask.list_formula()? {
         let cask_info = package.cask.ok_or_else(|| {
@@ -15,8 +27,22 @@ pub async fn list(cask: &cask::Cask) -> Result<(), Report> {
             )
         })?;
 
-        println!("{} {}", cask_info.name, cask_info.version);
+        let create_at = DateTime::parse_from_str(&cask_info.created_at, "%+")
+            .unwrap()
+            .format("%Y-%m-%d %H:%M:%S")
+            .to_string();
+
+        packages.push(PackageInfo {
+            name: cask_info.name,
+            bin: package.package.bin,
+            version: cask_info.version,
+            install_at: create_at,
+        });
     }
+
+    let table = Table::new(packages).with(Style::psql()).to_string();
+
+    println!("{}", table);
 
     Ok(())
 }

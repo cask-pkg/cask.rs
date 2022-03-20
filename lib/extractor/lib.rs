e24d629e2573ea1821229a99b22c25697938fff4
Axetroy <axetroy.dev@gmail.com>
2022-03-20 23:45:30 +0800

    refactor: improve extractor error message

--- lib/extractor/lib.rs
+++ lib/extractor/lib.rs
@@ -8,58 +8,84 @@ mod zip;
 
 use core::result::Result;
 use std::fs;
+use std::io;
 use std::path::Path;
 use std::path::PathBuf;
 
 use eyre::Report;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ExtractorError {
+    #[error("can not create folder '{folder:?}': {source:?}")]
+    CreateFolderFail { folder: String, source: io::Error },
+    #[error("can not found file '{filename:?}' in path '{path:?}' for tarball")]
+    FindNotFoundInTarball { filename: String, path: String },
+    #[error("not support extract file from '{filename:?}'")]
+    NotSupportExtension { filename: String },
+    #[error("extract file '{filename:?}' in '{path:?}' error: {msg:?}")]
+    ExtractFail {
+        filename: String,
+        path: String,
+        msg: String,
+    },
+}
 
 pub fn extract(
     tarball: &Path,
     dest_dir: &Path,
     filename: &str,
     folder: &str,
-) -> Result<PathBuf, Report> {
+) -> Result<PathBuf, ExtractorError> {
     let tar_file_name = tarball.file_name().unwrap().to_str().unwrap();
 
     let ensure_extract_file_exist = |s: &Path| {
         if s.exists() && s.is_file() {
             Ok(s.to_owned())
         } else {
-            Err(eyre::format_err!(
-                "can not found file '{}' in the '{}' of tarball",
-                &filename,
-                folder
-            ))
+            Err(ExtractorError::FindNotFoundInTarball {
+                filename: filename.to_string(),
+                path: folder.to_string(),
+            })
         }
     };
 
-    fs::create_dir_all(dest_dir)
-        .map_err(|e| eyre::format_err!("can not create folder '{}': {}", dest_dir.display(), e))?;
+    let handle_extract_error = |e: Report| {
+        Err(ExtractorError::ExtractFail {
+            filename: filename.to_string(),
+            path: folder.to_string(),
+            msg: format!("{}", e),
+        })
+    };
+
+    fs::create_dir_all(dest_dir).map_err(|e| ExtractorError::CreateFolderFail {
+        folder: format!("{}", dest_dir.display()),
+        source: e,
+    })?;
 
     if tar_file_name.ends_with(".tar.gz") || tar_file_name.ends_with(".tgz") {
         match tgz::extract(tarball, dest_dir, filename, folder) {
             Ok(p) => ensure_extract_file_exist(&p),
-            Err(e) => Err(e),
+            Err(e) => handle_extract_error(e),
         }
     } else if tar_file_name.ends_with(".tar.bz2") {
         match tbz2::extract(tarball, dest_dir, filename, folder) {
             Ok(p) => ensure_extract_file_exist(&p),
-            Err(e) => Err(e),
+            Err(e) => handle_extract_error(e),
         }
     } else if tar_file_name.ends_with(".tar") {
         match tar::extract(tarball, dest_dir, filename, folder) {
             Ok(p) => ensure_extract_file_exist(&p),
-            Err(e) => Err(e),
+            Err(e) => handle_extract_error(e),
         }
     } else if tar_file_name.ends_with(".zip") {
         match zip::extract(tarball, dest_dir, filename, folder) {
             Ok(p) => ensure_extract_file_exist(&p),
-            Err(e) => Err(e),
+            Err(e) => handle_extract_error(e),
         }
     } else {
-        Err(eyre::format_err!(
-            "not support extract file from '{}'",
-            tar_file_name
-        ))
+        Err(ExtractorError::NotSupportExtension {
+            filename: tar_file_name.to_string(),
+        })
     }
 }

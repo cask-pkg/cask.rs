65888c0a405307358af3f7011730ef975f2edd54
Axetroy <axetroy.dev@gmail.com>
2022-03-22 02:38:39 +0800

    update

--- lib/downloader/lib.rs
+++ lib/downloader/lib.rs
@@ -6,64 +6,63 @@ use std::{cmp::min, fs, fs::File, io::Write, path::Path};
 use eyre::Report;
 use futures_util::StreamExt;
 use indicatif::{ProgressBar, ProgressStyle};
-#[cfg(feature = "reqwest")]
-use reqwest::Client;
 
-#[cfg(feature = "reqwest")]
 pub async fn download(url: &str, filepath: &Path) -> Result<(), Report> {
-    let client = &Client::new();
+    if cfg!(feature = "lib") {
+        let client = &reqwest::Client::new();
 
-    let res = client.get(url).send().await?;
+        let res = client.get(url).send().await?;
 
-    if res.status() != 200 {
-        return Err(eyre::format_err!(
-            "Download {} fail with http code {}",
-            &url,
-            res.status()
-        ));
+        if res.status() != 200 {
+            return Err(eyre::format_err!(
+                "Download {} fail with http code {}",
+                &url,
+                res.status()
+            ));
+        }
+
+        let total_size = res
+            .content_length()
+            .ok_or_else(|| eyre::format_err!("Failed to get content length from {}", &url))?;
+
+        let progress_template = "{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})";
+        let pb = ProgressBar::new(total_size);
+        pb.set_style(
+            ProgressStyle::default_bar()
+                .template(progress_template)
+                .progress_chars("#>-"),
+        );
+        pb.set_message(format!("Downloading {}", url));
+
+        if filepath.exists() {
+            fs::remove_file(filepath)?;
+        }
+
+        let mut dest = File::create(filepath)?;
+
+        let mut downloaded: u64 = 0;
+        let mut stream = res.bytes_stream();
+
+        while let Some(item) = stream.next().await {
+            let chunk = item.map_err(|_| eyre::format_err!("Error while downloading file"))?;
+
+            dest.write_all(&chunk)
+                .map_err(|_| eyre::format_err!("Error while write file"))?;
+
+            downloaded = min(downloaded + (chunk.len() as u64), total_size);
+
+            pb.set_position(downloaded);
+        }
+
+        pb.finish();
+
+        Ok(())
+    } else if cfg!(feature = "cmd") {
+        println!("download {} to {}", url, filepath.display());
+        Ok(())
+    } else {
+        todo!()
     }
-
-    let total_size = res
-        .content_length()
-        .ok_or_else(|| eyre::format_err!("Failed to get content length from {}", &url))?;
-
-    let progress_template = "{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})";
-    let pb = ProgressBar::new(total_size);
-    pb.set_style(
-        ProgressStyle::default_bar()
-            .template(progress_template)
-            .progress_chars("#>-"),
-    );
-    pb.set_message(format!("Downloading {}", url));
-
-    if filepath.exists() {
-        fs::remove_file(filepath)?;
-    }
-
-    let mut dest = File::create(filepath)?;
-
-    let mut downloaded: u64 = 0;
-    let mut stream = res.bytes_stream();
-
-    while let Some(item) = stream.next().await {
-        let chunk = item.map_err(|_| eyre::format_err!("Error while downloading file"))?;
-
-        dest.write_all(&chunk)
-            .map_err(|_| eyre::format_err!("Error while write file"))?;
-
-        downloaded = min(downloaded + (chunk.len() as u64), total_size);
-
-        pb.set_position(downloaded);
-    }
-
-    pb.finish();
-
-    Ok(())
-}
-
-#[cfg(feature = "cmd")]
-pub async fn download(url: &str, filepath: &Path) -> Result<(), Report> {
-    Ok(())
 }
 
 #[cfg(test)]

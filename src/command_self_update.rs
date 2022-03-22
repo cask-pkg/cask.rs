96faf0b4c1cd89391ba45a1f9125a173cd1cecdd
Axetroy <axetroy.dev@gmail.com>
2022-03-22 19:22:58 +0800

    build for powerpc64

--- src/command_self_update.rs
+++ src/command_self_update.rs
@@ -33,6 +33,18 @@ fn get_arch() -> String {
     {
         "mips64el".to_string()
     }
+    #[cfg(target_arch = "powerpc")]
+    {
+        "powerpc".to_string()
+    }
+    #[cfg(target_arch = "powerpc64")]
+    {
+        "powerpc64".to_string()
+    }
+    #[cfg(target_arch = "powerpc64el")]
+    {
+        "powerpc64el".to_string()
+    }
     #[cfg(target_arch = "riscv64")]
     {
         "riscv64gc".to_string()

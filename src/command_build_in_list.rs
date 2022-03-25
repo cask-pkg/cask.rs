895dfde5f74aec1bdf70c9e5dde9122101b1c8c1
Axetroy <axetroy.dev@gmail.com>
2022-03-26 02:11:53 +0800

    add cask build-in command

--- src/command_build_in_list.rs
+++ src/command_build_in_list.rs
@@ -1,17 +1,35 @@
 #![deny(warnings)]
 
-use crate::{cask, command_build_in_sync};
+use std::{fs, path::Path};
+
+use crate::{cask, command_build_in_sync, formula};
 
 use eyre::Report;
+
+fn print_formula(dir_path: &Path) -> Result<(), Report> {
+    let dir = fs::read_dir(dir_path)?;
+
+    for entry in dir.into_iter().filter(|f| f.is_ok()).map(|f| f.unwrap()) {
+        let p = entry.path();
+
+        if p.is_dir() {
+            print_formula(&p)?
+        } else if entry.file_name().to_str().unwrap() == "Cask.toml" {
+            let f = formula::new(&p, "")?;
+
+            println!("{}", f.package.name)
+        }
+    }
+
+    Ok(())
+}
 
 pub fn list(cask: &cask::Cask) -> Result<(), Report> {
     let mirror_dir = cask.build_in_formula_dir();
 
-    if mirror_dir.exists() {
+    if !mirror_dir.exists() {
         command_build_in_sync::sync(cask)?;
     }
 
-    // TODO
-
-    Ok(())
+    print_formula(&mirror_dir)
 }

d1a4d51c894f3d65386074af41b746feb48791b3
Axetroy <axetroy.dev@gmail.com>
2022-11-24 02:46:40 +0800

    update

--- src/command_relink.rs
+++ src/command_relink.rs
@@ -2,6 +2,8 @@
 
 use crate::{cask, symlink};
 
+use std::fs;
+
 use eyre::Report;
 
 pub async fn relink(cask: &cask::Cask) -> Result<(), Report> {
@@ -18,6 +20,14 @@ pub async fn relink(cask: &cask::Cask) -> Result<(), Report> {
         let executable_name = format!("{}.exe", &package_formula.package.bin);
 
         let output_file_path = package_dir.join("bin").join(executable_name);
+
+        // unlink before symlink
+        {
+            fs::remove_file(&symlink_file).ok();
+
+            #[cfg(target_family = "windows")]
+            fs::remove_file(format!("{}.bat", &symlink_file)).ok();
+        }
 
         symlink::symlink(
             &output_file_path,
